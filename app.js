f253e6580bb4b24cce24ef94b89714791a3d1af8
bazinygero <97780599+bazinygero@users.noreply.github.com>
2022-04-06 22:44:16 +0300

    initialize, api add, so on

---
+++ app.js
@@ -0,0 +1,46 @@
+const puzzleBoard = document.querySelector('#puzzle')
+const solveButton = document.querySelector('#solve-btn')
+const squares = 81
+const submission = []
+
+for(let i = 0; i<squares; i++) {
+    const inputElement = document.createElement('input')
+    inputElement.setAttribute('type', 'number')
+    inputElement.setAttribute('min', '1')
+    inputElement.setAttribute('max', '9')
+    puzzleBoard.appendChild(inputElement)
+}
+
+const joinValues = () => {
+   const inputs = document.querySelectorAll('input')
+   inputs.forEach(input => {
+       if(input.value) {
+            submission.push(input.value)
+       } else {
+           submission.push('.')
+       }
+   })
+   console.log(submission)
+}
+
+const solve = () => {
+
+    const options = {
+        method: 'POST',
+        url: 'https://solve-sudoku.p.rapidapi.com/',
+        headers: {
+          'content-type': 'application/json',
+          'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com',
+          'X-RapidAPI-Key': 'e9898f454amsh348c58f50e9ef58p1c865fjsn0d7a47e6cc94'
+        },
+        data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}'
+      };
+      
+      axios.request(options).then((response) => {
+          console.log(response.data)
+      }).catch((error) => {
+          console.error(error)
+      })
+}
+
+solveButton.addEventListener('click', solve) 
